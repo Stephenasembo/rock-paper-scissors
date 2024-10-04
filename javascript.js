3715bb1bc30ddafac16b462e467ac040e4d08e02
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-10-04 10:58:41 +0300

    Get Computer Choice
    The computer will act as a counterparty for the user
    in the game

--- javascript.js
+++ javascript.js
@@ -3,7 +3,24 @@
 // Declare score variables
 // In each round
     // Get computer choice randomly
-    // Get human choice
+    function getComputerChoice()
+    {
+        let randomNumber = Math.random()
+        if (randomNumber > 0 && randomNumber < 0.33)
+        {
+            choice = 'rock';
+        }
+        else if (randomNumber > 0.33 && randomNumber < 0.66)
+        {
+            choice = 'paper';
+        }
+        else
+        {
+            choice = 'scissors'
+        }
+        return choice;
+    }
+        // Get human choice
         // Prompt user for input
             // Make it case insensitive
             // Handle input errors
