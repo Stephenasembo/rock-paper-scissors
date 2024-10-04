52cbb1822c5c1d461acb066aee4fe86f191c274c
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-10-04 10:48:58 +0300

    Add Psuedocode
    Provides step by step logic of the program

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+// Play the game
+// The game has 5 rounds
+// Declare score variables
+// In each round
+    // Get computer choice randomly
+    // Get human choice
+        // Prompt user for input
+            // Make it case insensitive
+            // Handle input errors
+    // Decide the winner
+    // Display the winner
+    // Increment the winner's score by 1
+// Play 4 more rounds
+// Decide the winner of the game based on their scores
+// Display the winner
